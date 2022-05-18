7755eb869b77259a9a86b2c4d7b2fbb2025ea2bc
AJ <necessarylion@gmail.com>
2022-05-18 14:21:34 +0700

    update script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import * as fs from 'fs'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import createDirectoryContents from './createDirectoryContents.js'
+import { exec } from 'child_process'
 const CURR_DIR = process.cwd()
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -26,6 +27,12 @@ inquirer.prompt(QUESTIONS).then((answers) => {
   const templatePath = `${__dirname}/templates`
 
   fs.mkdirSync(`${CURR_DIR}/${projectName}`)
-
   createDirectoryContents(templatePath, projectName)
+
+  console.log('◕ Running npm install...')
+
+  exec(`cd ${projectName} && npm install`, (error, stdout) => {
+    console.log('\x1b[32m✓ Successfully created Kernode project')
+    console.log(`\x1b[33mcd ${projectName} && npm run dev`)
+  })
 })
